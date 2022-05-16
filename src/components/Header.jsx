614c4cc6340330e1a18bc2d4b8ecf9a6a1c30435
Atefeh-Masoumi <atefeh.masoomy@gmail.com>
2022-05-17 00:19:24 +0430

    mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,13 +15,13 @@ const Header = () => {
             {/* nav */}
             <div className="flex w-full relative h-8  top-0  ">
                 <div className="pl-14  ">
-                    <div className="bg-yellow-400 rounded-b-lg px-3   ">
+                    <div className="bg-yellow-400 rounded-b-lg px-3 hidden md:flex">
                         <span>خرید از مارکت راست چین </span>
                     </div>
 
                 </div>
                 
-                <div className="bg-slate-100" >
+                <div className="bg-slate-100 hidden md:flex" >
                     
                     <div>
 
@@ -32,13 +32,26 @@ const Header = () => {
 
             </div>
             {/* end nav */}
+            {/* mobile menue */}
+            <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+          <li className='border-b-2 border-zinc-300 w-full' onClick={handleClose} to="home" smooth={true} duration={500}>Home</li>
+          <li className='border-b-2 border-zinc-300 w-full' onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</li>
+          <li className='border-b-2 border-zinc-300 w-full' onClick={handleClose} to="support" smooth={true} offset={-50} duration={500}>Support</li>
+          <li className='border-b-2 border-zinc-300 w-full' onClick={handleClose} to="platforms" smooth={true} offset={-100} duration={500}>Platforms</li>
+          <li className='border-b-2 border-zinc-300 w-full' onClick={handleClose} to="pricing" smooth={true} offset={-50} duration={500}>Pricing</li>
+
+        <div className='flex flex-col my-4'>
+            <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>icon</button>
+            <button className='px-8 py-3'>copy right</button>
+        </div>
+      </ul>
 
             {/* start of headerpng files */}
                 <div className="lg:h-[120px] ">
 
                 </div>
 
-                <div className="flex flex-1 items-center" >
+                <div className="flex flex-1 items-center " >
                     <div className="z-9 w-96 h-80 px-2 m-4 " >
                         <img src={images.crane} alt="crane" className="color"/>
 
@@ -50,14 +63,14 @@ const Header = () => {
                         </div>
 
                         <div className="text-white ">
-                            <span className="text-3xl font-bold">بهترین قالب مهندسی، ساختمانی وردپرس </span>
+                            <span className="lg:text-3xl font-bold">بهترین قالب مهندسی، ساختمانی وردپرس </span>
                         </div>
                         <div className="text-white ">
-                            <span className="text-3xl font-bold">وب سایت اماده کاملا فارسی سازی شده <span>12</span> </span>
+                            <span className="lg:text-3xl font-bold">وب سایت اماده کاملا فارسی سازی شده <span>12</span> </span>
                         </div>
                         <div className="bg-slate-700 flex flex-row ">
                             
-                            <span className="text-3xl text-white font-bold ">سازگار با صفحه ساز محبوب و قدرتمند المنتور</span>
+                            <span className="lg:text-3xl text-white font-bold ">سازگار با صفحه ساز محبوب و قدرتمند المنتور</span>
                             <img src={images.elementor} alt="" className="h-16 w-16"/>
                         </div>
                     </div>
