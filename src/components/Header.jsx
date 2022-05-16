a8265fff062aaf9770883ecbe1152019aba56377
Atefeh-Masoumi <atefeh.masoomy@gmail.com>
2022-05-16 22:02:01 +0430

    nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,33 @@
-const Header = () => {
-    return ( 
-        <div style= {{backgroundImage:"url('../images/Blackground.jpg')",}}>
+import { images } from "../constant";
+import { useState } from "react";
 
+const Header = () => {
+   
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
+    const handleClose =()=> setNav(!nav)
+    return ( 
+        <div className="w-full ">
+            <div className="absolute bg-transparent px-0 m-0 block top-0 overflow-hidden mix-blend-overlay" >
+             <img src={images.bg} alt="bgcolor" className="object-cover mix-blend-overlay"/>
+            </div>
+
+            <div className="flex w-full relative h-8  top-0  ">
+                <div className="pl-7">
+                    <div className="bg-yellow-400 ">
+                        <span>خرید از مارکت راست چین </span>
+                    </div>
+
+                </div>
+                <div>
+
+                </div>
+
+            </div>
         </div>
+
+       
      );
 }
  
